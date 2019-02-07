3b2e35d738e978b4bc8e22bdd4ce7ba4ad9cf4c4
Lizhou Cai <cail3@rpi.edu>
2019-02-07 16:40:02 -0500

    menuState

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(String level)
+    {
+        if (level == "MenuState")
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitGame()
+    {
+
+        //save game!!!
+
+    #if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+
+    #endif
+    }
+}
