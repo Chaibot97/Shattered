4e620e66326d914d22c6894dfc418a3f5b6a2f54
Lizhou Cai <cail3@rpi.edu>
2019-04-02 22:26:31 -0400

    ending

--- Assets/Ending.cs
+++ Assets/Ending.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Ending : MonoBehaviour {
     public Filter filter;
-
+    public MirrorReflection m1;
+    public MirrorReflection m2;
 
     void Start () {
 		
@@ -13,5 +14,15 @@ public class Ending : MonoBehaviour {
 	public void Blink()
     {
         filter.blink();
+    }
+    public void ChangeMirror()
+    {
+        StartCoroutine(ChangeMirrorHelper());
+    }
+    public IEnumerator ChangeMirrorHelper()
+    {
+        if (m2) m2.enabled=true;
+        yield return new WaitForSeconds(0.2f);
+        if (m1) m1.enabled = false;
     }
 }
--- Assets/EndingTrigger.cs
+++ Assets/EndingTrigger.cs
@@ -7,8 +7,12 @@ public class EndingTrigger : MonoBehaviour {
     public float delay;
     public SceneLoader sl;
     public Text prompt;
+    public GameObject man;
+
     public void End()
     {
+        man.SetActive(true);
+        GetComponent<MirrorReflection>().m_TextureSize = 1024;
         EndingCamera.enabled = true;
         StartCoroutine(EndRoutine());
     }
@@ -17,6 +21,7 @@ public class EndingTrigger : MonoBehaviour {
     {
         EndingCamera.GetComponent<Animator>().SetTrigger("end");
         yield return new WaitForSeconds(delay);
+        Debug.Log("go");
         prompt.text = "Press any key to continue...";
         while (!Input.anyKey)
         {
